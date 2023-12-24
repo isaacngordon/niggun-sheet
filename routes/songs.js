bc0c13857b524b85d587d19bf7ac468d07b5b533
mimzy <tzachi@diysimchas.com>
2023-12-24 14:31:47 -0500

    csv parsing correctly w quoted newlines n commas

--- routes/songs.js
+++ routes/songs.js
@@ -16,13 +16,19 @@ router.get('/', (req, res) => {
         }
 
         // Parse the CSV data and convert it to JSON
-        const songs = data.split('\n').map((line) => {
-            console.log(line);
-            const [title, lyrics, artist] = line.split(',');
+        const songs = data.split(/\r?\n(?=(?:(?:[^"]*"){2})*[^"]*$)/).map((line, i) => {
+            // Skip the first line (headers)
+            if (i === 0) return;
+            // Skip the last line (if empty)
+            if (!line) return;
+            const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
+            const [title, lyrics, artist] = line.split(regex).map(cell => cell.replace(/"/g, ''));
             return { title, lyrics, artist };
         });
 
-        res.json(songs);
+        // remove the first element in the array if it is null otherwise return the array
+        console.log(songs[0] == null ? songs.slice(1) : songs);
+        res.json(songs[0] == null ? songs.slice(1) : songs);
     });
 });
 
