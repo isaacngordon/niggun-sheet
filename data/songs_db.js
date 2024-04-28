2b6d9989cd73337eb2b19ab683dc6030e5aee996
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2024-04-27 23:40:50 -0400

    rename file string

--- data/songs_db.js
+++ data/songs_db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./data/mydatabase.db', (err) => {
+const db = new sqlite3.Database('./db.db', (err) => {
   if (err) {
     console.error('Error opening database ' + err.message);
   } else {
