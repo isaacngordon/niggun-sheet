c77e8fab7b278af7ad9e2e288f6e31fc33c56ef0
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2024-04-27 23:37:01 -0400

    getting ready to switch to db from csv

---
+++ data/songs_db.js
@@ -0,0 +1,13 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./data/mydatabase.db', (err) => {
+  if (err) {
+    console.error('Error opening database ' + err.message);
+  } else {
+    console.log('Database connected!');
+  }
+});
+
+
+// export the database connection
+module.exports = db;
