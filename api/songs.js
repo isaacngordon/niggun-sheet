61375e2a938180521c6299c37e9c647cc996ef7d
yehjac22 <90438069+yehjac22@users.noreply.github.com>
2025-06-22 05:33:01 -0400

    tried fix for songs.js

--- api/songs.js
+++ api/songs.js
@@ -3,11 +3,11 @@ const app = express();
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const { getAllRows } = require('../data/db');
 
 const songsFilePath = path.join(__dirname, '../data/songs.csv');
 
-function csv_handler(req, res) {
+// obtains the song data from the csv file
+function handler(req, res) {
     //log request 
     console.log(`Handling GET /songs ${req.query} from ${req.ip}`);
 
@@ -25,7 +25,7 @@ function csv_handler(req, res) {
             if (!line) return;
             const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
             const [search_title, title, lyrics, artist, drive, youtube] = line.split(regex).map(cell => cell.replace(/"/g, ''));
-            return { search_title, title, lyrics, artist, drive, youtube };
+            return {search_title,  title, lyrics, artist, drive, youtube };
         });
 
         // remove the first element in the array if it is null otherwise return the array
@@ -34,25 +34,7 @@ function csv_handler(req, res) {
     });
 }
 
-async function get_all_songs_handler(req, res) {
-    //log request 
-    console.log(`Handling GET /songs ${req.query} from ${req.ip}`);
-
-    try {
-        const stmt = fs.readFileSync(path.join(__dirname, '../data/sql/get_song_list.sql'), 'utf-8');
-        console.log(stmt);
-        const rows = await getAllRows(stmt).catch(err => { throw err; });
-        console.log(rows);
-        res.json(rows);
-    }
-    catch (err) {
-        console.error("Error in get_all_songs_handler: ", err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-app.get('/', get_all_songs_handler);
-app.get('/csv', csv_handler);
+app.get('/api/songs', handler);
 
 
 module.exports = app;
